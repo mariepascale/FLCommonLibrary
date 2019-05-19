c6d1bb82cebc8dc539b9aa49914259926180dc03
mariepascale <marie.pascale.chauvel@gmail.com>
2019-05-19 01:53:26 -0400

    update service

--- FLCommonLibrary/Classes/ReplaceMe.swift
+++ FLCommonLibrary/Classes/ReplaceMe.swift
@@ -1,9 +1,1 @@
-import Foundation
 
-public class ReplaceMe {
-    private init() {}
-    
-    public static func doSomething() -> String {
-        return "Did Some Stuff"
-    }
-}
---
+++ ServiceTest.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceTest.swift
+//  FLCommonLibrary
+//
+//  Created by Marie-Pascale Chauvel on 5/19/19.
+//
+
+import Foundation
+
+public class ServiceTest {
+    private init() {}
+    
+    public static func doSomethingAgain() -> String {
+        return "Did Some Stuff Again"
+    }
+}
+
